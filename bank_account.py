1526d9dafa451a054ed600d91327f0583f6315db
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-01-25 23:27:04 -0800

    Created docstrings and README.md

--- bank_account.py
+++ bank_account.py
@@ -9,6 +9,13 @@ class BankAccount:
         self.balance = 0
 
     def account_num_randomizer(self):
+        '''
+        Generate a unique 8 digit numeric string. 
+        '''
+        # Create 8 digit number by looping through a random number from 0-9 using random.randint
+        # and adding them together while a count gets added by 1 each loop -- loop stops when 
+        # count is 8. checks if 8 digit number has been used. If not, will add the number to a
+        # list of used numbers and return the number.
         while True:
             count = 0
             number = ''
@@ -18,27 +25,50 @@ class BankAccount:
 
             if number not in self.registered_accounts_num:
                 self.registered_accounts_num.append(number)
-                return int(number)
+                return number
             
     def deposit(self, amount):
+        '''
+        Add an amount to BankAccount object balance and display amount and new balance.
+        
+        :param amount: Deposit amount -- amount added to balance
+        :type amount: int
+        '''
         self.balance += amount
         print(f'Amount deposited: ${amount} New Balance: ${round(self.balance, 2)}')
 
     def withdraw(self, amount):
+        '''
+        Subtract an amount to BankAccount object balance and display amount and new balance.
+        If amount more than object balance, subtract additional 10 to simulate overdraft fee
+        and display alert of overdraft for insufficient funds.
+        
+        :param amount: Withdraw amount -- amount subtracted from balance
+        :type amount: int
+        '''
         if amount > self.balance:
             self.balance -= amount
             self.balance -= 10
-            print(f'Insufficient funds. Incured $10 overdraft fee')
+            print(f'Insufficient funds. Inccured $10 overdraft fee')
             print(f'Balance: {round(self.balance, 2)}')
         else: 
             self.balance -= amount
             print(f'Amount deposited: ${amount} New Balance: ${round(self.balance, 2)}')
 
     def get_balance(self):
+        '''
+        Print BankAccount object balance and return balance.
+
+        :return balance: BankAccount object balance
+        '''
         print(f'Balance: ${round(self.balance, 2)}')
         return self.balance
     
     def add_interest(self):
+        '''
+        Add interest of 0.83% to BankAccount object balance if balance is greater than 0
+        and print new balance. If not greater, print alert of insufficient funds. 
+        '''
         if self.balance > 0:
             self.balance = self.balance * 1.00083
             print(f'Balance: ${round(self.balance, 2)}')
@@ -46,7 +76,11 @@ class BankAccount:
             print('Get out of debt first')
 
     def print_statement(self):
-        account_number_hidden = '****' + str(self.account_number)[-4:]
+        '''
+        Print BankAccount object full name, hidden bank account number showing only last 4 digits,
+        and balance rounded to 2 decimal points.
+        '''
+        account_number_hidden = '****' + self.account_number[-4:]
         print(f'{self.full_name}')
         print(f'Account No.: {account_number_hidden}')
         print(f'Balance: ${round(self.balance, 2)}')
